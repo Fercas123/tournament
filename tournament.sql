5fdbd93ad7acb72646df74fef23133f59c19bc27
fernanda castillo <fersitacastillo123@gmail.com>
2016-09-26 02:49:03 +0100

    drop and create database added

--- tournament.sql
+++ tournament.sql
@@ -3,6 +3,10 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+\c tournament
 
 CREATE TABLE players ( id SERIAL PRIMARY KEY , name TEXT);
 CREATE TABLE matches ( id SERIAL PRiMARY KEY,
